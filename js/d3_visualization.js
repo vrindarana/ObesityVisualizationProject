ee633268cce1a377281d3a24945e8f3664dd1efd
Emma Mathew <123320407+emmamathew3@users.noreply.github.com>
2025-04-08 11:21:04 -0400

    Update d3_visualization.js

--- js/d3_visualization.js
+++ js/d3_visualization.js
@@ -1,114 +1,131 @@
-const margin = { top: 70, right: 150, bottom: 60, left: 70 },
-      width = 900 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
+const width = 800;
+const height = 500;
+const margin = { top: 50, right: 100, bottom: 70, left: 70 };
 
 const svg = d3.select("#chart")
   .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .style("font-family", "'EB Garamond', serif")
-  .append("g")
-  .attr("transform", `translate(${margin.left},${margin.top})`);
+  .attr("width", width)
+  .attr("height", height);
+
+const colorScale = d3.scaleOrdinal()
+  .domain([
+    'Insufficient_Weight',
+    'Normal_Weight',
+    'Overweight_Level_I',
+    'Overweight_Level_II',
+    'Obesity_Type_I',
+    'Obesity_Type_II',
+    'Obesity_Type_III'
+  ])
+  .range([
+    '#88CCEE',  
+    '#DDCC77',  
+    '#CC6677',  
+    '#117733',  
+    '#332288', 
+    '#AA4499',  
+    '#44AA99'   
+  ]);
 
 d3.csv("Obesity prediction.csv").then(data => {
-  const counts = d3.rollups(
+  const nestedData = d3.rollups(
     data,
     v => v.length,
     d => d.CAEC,
-    d => d.Obesity
+    d => d.NObesity
   );
 
-  const stackedData = counts.map(([caec, levels]) => {
-    const row = { CAEC: caec };
-    levels.forEach(([obesity, count]) => {
-      row[obesity] = count;
+  const formatted = nestedData.map(([caec, values]) => {
+    const obj = { CAEC: caec };
+    values.forEach(([obesity, count]) => {
+      obj[obesity] = count;
     });
-    return row;
+    return obj;
   });
 
-  const keys = Array.from(new Set(data.map(d => d.Obesity)));
+  const obesityLevels = colorScale.domain();
 
-  const x = d3.scaleBand()
-    .domain(["no", "Sometimes", "Frequently", "Always"])
-    .range([0, width])
-    .padding(0.2);
+  const stackedData = d3.stack()
+    .keys(obesityLevels)
+    (formatted);
 
-  const y = d3.scaleLinear()
-    .domain([0, d3.max(stackedData, d => d3.sum(keys, k => d[k] || 0))])
-    .nice()
-    .range([height, 0]);
+  const xScale = d3.scaleBand()
+    .domain(formatted.map(d => d.CAEC))
+    .range([margin.left, width - margin.right])
+    .padding(0.3);
 
-  const color = d3.scaleOrdinal()
-    .domain(keys)
-    .range(d3.schemeGreens[keys.length]);
+  const yMax = d3.max(stackedData, series =>
+    d3.max(series, d => d[1])
+  );
 
-  const stacked = d3.stack()
-    .keys(keys)
-    .value((d, key) => d[key] || 0)(stackedData);
-
-  svg.append("g")
-    .attr("transform", `translate(0,${height})`)
-    .call(d3.axisBottom(x))
-    .selectAll("text")
-    .style("font-size", "14px");
+  const yScale = d3.scaleLinear()
+    .domain([0, yMax])
+    .range([height - margin.bottom, margin.top]);
 
   svg.append("g")
-    .call(d3.axisLeft(y))
+    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .call(d3.axisBottom(xScale))
     .selectAll("text")
+    .style("font-family", "'EB Garamond', serif")
+    .style("font-size", "13px");
+
+  svg.append("g")
+    .attr("transform", `translate(${margin.left},0)`)
+    .call(d3.axisLeft(yScale))
+    .selectAll("text")
+    .style("font-family", "'EB Garamond', serif")
     .style("font-size", "13px");
 
   svg.append("text")
-    .attr("transform", "rotate(-90)")
     .attr("x", -height / 2)
-    .attr("y", -margin.left + 20)
-    .attr("text-anchor", "middle")
-    .style("font-size", "14px")
+    .attr("y", 20)
+    .attr("transform", "rotate(-90)")
+    .style("text-anchor", "middle")
+    .style("font-family", "'EB Garamond', serif")
+    .style("font-size", "16px")
     .text("Number of Individuals");
 
-  const tooltip = d3.select("body")
-    .append("div")
-    .attr("class", "tooltip")
-    .style("visibility", "hidden");
+  svg.append("text")
+    .attr("x", width / 2)
+    .attr("y", margin.top / 2)
+    .attr("text-anchor", "middle")
+    .style("font-family", "'EB Garamond', serif")
+    .style("font-size", "20px")
+    .style("font-weight", "bold")
+    .text("Figure 2: Food Between Meals vs Obesity (CAEC vs Obesity Level)");
 
   svg.selectAll("g.layer")
-    .data(stacked)
-    .join("g")
-    .attr("fill", d => color(d.key))
-    .selectAll("rect")
-    .data(d => d)
-    .join("rect")
-    .attr("x", d => x(d.data.CAEC))
-    .attr("y", d => y(d[1]))
-    .attr("height", d => y(d[0]) - y(d[1]))
-    .attr("width", x.bandwidth())
-    .on("mouseover", function (event, d) {
-      const obesity = d3.select(this.parentNode).datum().key;
-      const count = d[1] - d[0];
-      tooltip
-        .html(`<strong>Obesity:</strong> ${obesity}<br><strong>CAEC:</strong> ${d.data.CAEC}<br><strong>Count:</strong> ${count}`)
-        .style("visibility", "visible");
-    })
-    .on("mousemove", event => {
-      tooltip
-        .style("top", `${event.pageY - 40}px`)
-        .style("left", `${event.pageX + 20}px`);
-    })
-    .on("mouseout", () => tooltip.style("visibility", "hidden"));
-
-  const legend = svg.selectAll(".legend")
-    .data(keys)
+    .data(stackedData)
     .enter()
     .append("g")
-    .attr("transform", (d, i) => `translate(${width + 20}, ${i * 22})`);
+    .attr("class", "layer")
+    .attr("fill", d => colorScale(d.key))
+    .selectAll("rect")
+    .data(d => d)
+    .enter()
+    .append("rect")
+    .attr("x", d => xScale(d.data.CAEC))
+    .attr("y", d => yScale(d[1]))
+    .attr("height", d => yScale(d[0]) - yScale(d[1]))
+    .attr("width", xScale.bandwidth());
 
-  legend.append("rect")
-    .attr("width", 15)
-    .attr("height", 15)
-    .attr("fill", d => color(d));
+  const legend = svg.append("g")
+    .attr("transform", `translate(${width - margin.right + 20}, ${margin.top})`);
 
-  legend.append("text")
-    .attr("x", 20)
-    .attr("y", 12)
-    .style("font-size", "13px")
-    .text(d => d);
+  obesityLevels.forEach((level, i) => {
+    const row = legend.append("g")
+      .attr("transform", `translate(0, ${i * 25})`);
+
+    row.append("rect")
+      .attr("width", 15)
+      .attr("height", 15)
+      .attr("fill", colorScale(level));
+
+    row.append("text")
+      .attr("x", 20)
+      .attr("y", 12)
+      .text(level)
+      .style("font-family", "'EB Garamond', serif")
+      .style("font-size", "13px");
+  });
 });
